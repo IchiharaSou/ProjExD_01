09cb785a52eab835da82c653c5e7a7a1c0646220
ichisou <c0b2201414@edu.teu.ac.jp>
2023-06-13 16:39:57 +0900

    問６

--- pygame_image.py
+++ pygame_image.py
@@ -15,11 +15,20 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+       
+        tmr += 1
+        x = tmr%1600
 
-        pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        screen.blit(bg_img, [-x, 0])
+
+        screen.blit(bg_img, [1600-x, 0])
+        
+        screen.blit(kt_imgs[tmr%2],[300,200])
+
+
+
+        pg.display.update()    
+        clock.tick(100)
 
 
 if __name__ == "__main__":
